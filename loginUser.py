0cfeb6d40d441e6512679b792801bc6ee093faee
Elena Hayfullina <ElenaH@interlink-ua.com>
2016-02-28 19:28:01 +0200

    refactoring extract method

--- loginUser.py
+++ loginUser.py
@@ -1,29 +1,23 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-
+import mysql.connector
 import requests
-
-from MySQLdb import connect
-
-# from recaptcha import RecaptchaClient
+import json
 
 app = Flask(__name__)
-
+database_name = 'users'
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
         email = request.form['email'] 
         password = request.form['password'] 
-        state = save_to_db(email, password)
-        # recaptcha_client = RecaptchaClient('6LfqyRgTAAAAAEMw_phaQfPjZbLRgnvuk2wkUGWE', '6LfqyRgTAAAAAMdZM4AwZCOoJPuE3DOaZ7SQJnb5')
-        # recaptcha_response = request.form['g-recaptcha-response']
-        # private_key = "6LfqyRgTAAAAAEMw_phaQfPjZbLRgnvuk2wkUGWE"
-        # remote_ip = request.remote_addr
-        # data = {'secret': private_key, 'response': recaptcha_response ,'remoteip': remote_ip}
-        # r = requests.post('https://www.google.com/recaptcha/api/siteverify', data=data)
-        #
+        recaptcha_response = request.form['g-recaptcha-response']
+
+        if validate_recaptcha_response(recaptcha_response) == True:
+            save_to_db(email, password)
+
         return render_template('afterlogin.html', data='Good:)) Your email: '+email+', password: '+password)
     
     elif request.method == 'GET':
@@ -31,18 +25,28 @@ def login():
         return render_template('login.html')
 
 
+def validate_recaptcha_response(recaptcha_response):
+    private_key = "6LfqyRgTAAAAAEMw_phaQfPjZbLRgnvuk2wkUGWE"
+    google_recaptcha_url = "https://www.google.com/recaptcha/api/siteverify"
+    params = {'response': recaptcha_response, 'secret': private_key}
+    response = requests.post(google_recaptcha_url, params=params)
+    recaptcha_data = json.loads(response.text)
+    success = recaptcha_data["success"]
+
+    return success
+
+
 def save_to_db(email, password):
-  db = connect_to_db()
+  db = connect_to_db(database_name)
   create_table_users_if_needed(db)
   save_user(db, email, password)
   db.close()
-  return 'OK:))'
 
 
-def connect_to_db():
-  return connect(user='root', password='root',
+def connect_to_db(database_name):
+  return mysql.connector.connect(user='root', password='root',
                               host='127.0.0.1',
-                              database='users')
+                              database=database_name)
 
 
 def create_table_users_if_needed(db):
